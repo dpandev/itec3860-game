02930992542e123144aecf8b07e7bc555c8026dd
David Panesiu <davidpanesiu@gmail.com>
2025-11-06 21:34:12 -0500

    feat: add Item, Inventory, and supporting enums. Add stub for CombatSession and Effect.

---
+++ src/main/java/avengers/model/combat/CombatSession.java
@@ -0,0 +1,8 @@
+package avengers.model.combat;
+
+/** Represents a combat session between a player and a monster. */
+public class CombatSession {
+
+  /** Constructs a new CombatSession with the specified player and monster. */
+  public CombatSession() {}
+}
---
+++ src/main/java/avengers/model/combat/EffectTiming.java
@@ -0,0 +1,10 @@
+package avengers.model.combat;
+
+/** Represents the timing of an effect in combat. */
+public enum EffectTiming {
+  ON_HIT,
+  ON_EQUIP,
+  PASSIVE,
+  ON_TURN,
+  ON_TURN_END
+}
